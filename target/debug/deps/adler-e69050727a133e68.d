17eadf20023461b429e30e6b65e6f19907a36439
Rakshit-gen <64471316+Rakshit-gen@users.noreply.github.com>
2024-06-03 18:40:49 +0530

    New project

---
+++ target/debug/deps/adler-e69050727a133e68.d
@@ -0,0 +1,8 @@
+E:\Forcollege\practice\rustpr\file_compress\target\debug\deps\libadler-e69050727a133e68.rmeta: C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\adler-1.0.2\src\lib.rs C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\adler-1.0.2\src\algo.rs
+
+E:\Forcollege\practice\rustpr\file_compress\target\debug\deps\libadler-e69050727a133e68.rlib: C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\adler-1.0.2\src\lib.rs C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\adler-1.0.2\src\algo.rs
+
+E:\Forcollege\practice\rustpr\file_compress\target\debug\deps\adler-e69050727a133e68.d: C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\adler-1.0.2\src\lib.rs C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\adler-1.0.2\src\algo.rs
+
+C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\adler-1.0.2\src\lib.rs:
+C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\adler-1.0.2\src\algo.rs:
---
+++ target/debug/deps/cfg_if-c62a9d2c5cc211fa.d
@@ -0,0 +1,7 @@
+E:\Forcollege\practice\rustpr\file_compress\target\debug\deps\libcfg_if-c62a9d2c5cc211fa.rmeta: C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
+
+E:\Forcollege\practice\rustpr\file_compress\target\debug\deps\libcfg_if-c62a9d2c5cc211fa.rlib: C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
+
+E:\Forcollege\practice\rustpr\file_compress\target\debug\deps\cfg_if-c62a9d2c5cc211fa.d: C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\HP\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs:
