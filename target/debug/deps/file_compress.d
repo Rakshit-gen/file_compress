845f7a658122bd39d460935c6c93f5021d11c4e1
Rakshit-gen <64471316+Rakshit-gen@users.noreply.github.com>
2024-06-03 18:26:43 +0530

    New project

---
+++ target/debug/deps/file_compress.d
@@ -0,0 +1,5 @@
+E:\Forcollege\practice\rustpr\file_compress\target\debug\deps\file_compress.exe: src\main.rs
+
+E:\Forcollege\practice\rustpr\file_compress\target\debug\deps\file_compress.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/file_compress.d
@@ -0,0 +1,1 @@
+E:\Forcollege\practice\rustpr\file_compress\target\debug\file_compress.exe: E:\Forcollege\practice\rustpr\file_compress\src\main.rs
